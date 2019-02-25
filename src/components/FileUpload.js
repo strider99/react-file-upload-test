3e5929412649bf45b316eb8fc72295f92e30c36c
strider99 <panigrahiomprakash@gmail.com>
2019-02-25 08:58:53 +0530

    add confirm dialog

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {confirmAlert} from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 export default class FileUpload extends Component {
   state={
@@ -10,22 +12,42 @@ export default class FileUpload extends Component {
   fileSubmitHandler = (e) =>{
     e.preventDefault();
     const data = new FormData(e.target);
-    console.log(data.getAll('general'));
+    // console.log(data.getAll('general'));
+    // add a confirm alert just for fun
+    confirmAlert({
+      title: "Confrm file upload ",
+      message: "Are you sure?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => {
+            let endpoint = "http://192.168.0.50:3030/upload/";
+            axios
+            .post(endpoint, data, {
+              onUploadProgress: ProgressEvent => {
+                this.setState({
+                  loaded: (ProgressEvent.loaded / ProgressEvent.total*100),
+                })
+              },
+            })
+            .then(res => {
+              console.log(res)
+            })
+          }
+        },
+        {
+          label: "No",
+          onClick: () => {
+            alert("file not uploaded");
+          }
+        }
+      ]
+
+    })
     // data.append('file', this.state.selectedFile, this.state.selectedFile.name);
     // let fileData = e.target.files[0];
     // console.log(fileData);
-    let endpoint = "http://192.168.0.50:3030/upload/";
-    axios
-    .post(endpoint, data, {
-      onUploadProgress: ProgressEvent => {
-        this.setState({
-          loaded: (ProgressEvent.loaded / ProgressEvent.total*100),
-        })
-      },
-    })
-    .then(res => {
-      console.log(res)
-    })
+
 
   }
 
